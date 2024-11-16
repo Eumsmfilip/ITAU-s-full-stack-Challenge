5aea6b2423e804e3e5f39f9a60c2041b5a9082f6
Filipe Rocha <filiperochasantos4@gmail.com>
2024-11-16 18:38:26 -0300

    migrations created

---
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE users (
+                 id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+                 name VARCHAR(150) NOT NULL,
+                 lastName VARCHAR(100) NOT NULL,
+                 userParticipation DOUBLE PRECISION NOT NULL);
