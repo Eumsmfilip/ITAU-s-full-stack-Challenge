cf646243f794341fea0801e085dcfe5cb86b5a87
Filipe Rocha <filiperochasantos4@gmail.com>
2024-11-17 12:19:05 -0300

    feat: add post method

--- src/main/java/com/itau/research/controller/UserController.java
+++ src/main/java/com/itau/research/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.itau.research.controller;
 
 import com.itau.research.domain.User;
+import com.itau.research.domain.UserRequestDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.itau.research.service.UserService;
@@ -21,6 +23,12 @@ public class UserController {
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity <User> createUser(@RequestBody UserRequestDTO userRequestDTO){
+        User newUser = userService.createUser(userRequestDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/itau/research/service/UserService.java
+++ src/main/java/com/itau/research/service/UserService.java
@@ -1,8 +1,10 @@
 package com.itau.research.service;
 
 import com.itau.research.domain.User;
+import com.itau.research.domain.UserRequestDTO;
 import org.springframework.stereotype.Service;
 import com.itau.research.repository.UserRepository;
+
 import java.util.List;
 
 @Service
@@ -16,6 +18,15 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public User createUser(UserRequestDTO userRequestDTO){
+        User newUser = new User(
+                userRequestDTO.name(),
+                userRequestDTO.lastName(),
+                userRequestDTO.userParticipation()
+        );
+        return userRepository.save(newUser);
     }
 
 }
