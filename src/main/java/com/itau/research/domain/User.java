fb029f660f22d635a14cef9ff0e8c60db4ff2488
Filipe Rocha <filiperochasantos4@gmail.com>
2024-11-17 12:06:20 -0300

    fix: rename db table to 'users', because the name 'user' was causing trouble

--- src/main/java/com/itau/research/domain/User.java
+++ src/main/java/com/itau/research/domain/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user_entity")
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/resources/db/migration/V1__create-user-table.sql
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -3,6 +3,6 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE users (
                  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
                  name VARCHAR(150) NOT NULL,
-                 lastName VARCHAR(100) NOT NULL,
-                 userParticipation DOUBLE PRECISION NOT NULL
+                 last_name VARCHAR(100) NOT NULL,
+                 user_participation DOUBLE PRECISION NOT NULL
                  );
